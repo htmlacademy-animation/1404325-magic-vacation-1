6d714da0d264b75c0ac71ef8b3662df828ff7fdb
Nikita Galichin <nogalichin@avito.ru>
2020-05-05 21:53:28 +0300

    module1-task2 onload transitions

---
+++ source/js/modules/onload.js
@@ -0,0 +1,13 @@
+export default () => {
+  function onload() {
+    document.body.classList.add(`body--loaded`);
+  }
+
+  if (document.readyState === `loading`) {
+    document.addEventListener(`DOMContentLoaded`, onload);
+  } else {
+    onload();
+  }
+};
+
+
